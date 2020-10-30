10b8facb814caa73f5af0d710817635e17d95e11
bhavik <kariyabhavik410@gmail.com>
2020-10-30 09:14:26 +0530

    this is new peoject

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('myangular10app app is running!');
+    expect(page.getTitleText()).toEqual('app app is running!');
   });
 
   afterEach(async () => {
