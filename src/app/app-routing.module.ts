28e2a08a0c53984999d6f08478f945fcd5436242
bhavik <kariyabhavik410@gmail.com>
2020-10-30 14:07:17 +0530

    add data from to form in db folder and display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddRestoComponent} from "./add-resto/add-resto.component";
+import {ListRestoComponent} from "./list-resto/list-resto.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {UpdateRestoComponent} from "./update-resto/update-resto.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:AddRestoComponent,
+    path:'add'
+  },
+  {
+    component:ListRestoComponent,
+    path:'list'
+  },
+  {
+    component:LoginComponent,
+    path:'login'
+  },
+  {
+    component:RegisterComponent,
+    path:'register'
+  },
+  {
+    component:UpdateRestoComponent,
+    path:'update'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
